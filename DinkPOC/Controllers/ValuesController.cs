8cda036d88605cf255b14ed5612f759b2148f05a
dbz <dbirt3168@outlook.com>
2018-02-20 11:36:11 -0500

    first commit: running with the default sample

---
+++ DinkPOC/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DinkToPdf;
+
+namespace DinkPOC.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var converter = new SynchronizedConverter(new PdfTools());
+
+            var output = converter.Convert(new HtmlToPdfDocument()
+            {
+                GlobalSettings =
+                {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings() { Top = 10 },
+                    Out = @"C:\temp\dinktopdf-test.pdf",
+                },
+                Objects =
+                {
+                    new ObjectSettings()
+                    {
+                        Page = "https://quick.med-enroll.com",
+                    },
+                }
+            });
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
